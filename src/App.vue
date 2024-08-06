69d5a09511a97133215f8e348a7756739ecac175
Jose Alberto Guevara Morales <5410jos@gmail.com>
2024-08-06 09:26:48 -0600

    Se hace cambios en el scroll para dispositivos pequeños

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,27 @@
 <template>
-  <div ref="element" :class="print ? 'w-[8.27in] h-[11.69in]' : 'w-screen h-screen'">
-    <div class="w-full h-full bg-green-200 flex justify-center items-center font-noto" :class="{ 'p-20': print }">
-      <div
-        class="bg-[rgba(255,255,255,0.5)] rounded-md shadow-[0_4px_30px_rgba(0,0,0,0.1)] border-[1px_solid_rgba(255,255,255,0.3)]">
-        <form class="h-full w-full flex flex-col gap-2 py-2 px-8" @submit.prevent="submit">
+  <div ref="element" :class="print ? 'w-[8.27in] h-[11.69in]' : 'w-screen min-h-screen'">
+    <div class="w-full min-h-screen flex justify-center items-center font-noto" :class="{ 'p-20': print }">
+      <div class="bg-[rgba(255,255,255,0.5)] rounded-md shadow-[0_4px_30px_rgba(0,0,0,0.1)] border-[1px_solid_rgba(255,255,255,0.3)] overflow-auto max-h-screen">
+        <form class="flex flex-col gap-2 py-2 px-8" @submit.prevent="submit">
           <span class="self-center text-xl font-bold uppercase py-2">Datos Del Cliente</span>
-          <InputText text="Persona de contacto:" placeholder="Nombre completo" name="contacto"
-            v-model="properties.contacto" />
-          <InputText text="Nombre del comercio:" placeholder="Nombre de fantasía" name="fantasia"
-            v-model="properties.fantasia" />
-          <InputText text="Razón social:" placeholder="Nombre físico o jurídico" name="razonsocial"
-            v-model="properties.razonsocial" />
-          <InputNumber text="Número de cédula física o jurídica:" placeholder="101230321" name="cedula"
-            v-model="properties.cedula" />
-          <RadioButton text="Régimen del negocio:" :options="['Simplificado', 'Tradicional']" name="regimen"
-            v-model="properties.regimen" />
-          <InputDatalist text="Tipo de negocio"
-            :options="['Soda', 'Restaurante', 'Cafeteria', 'Supermercado', 'Salon de belleza', 'Tienda de ropa']"
-            name="tiponegocio" v-model="properties.tiponegocio"></InputDatalist>
-          <RadioButton text="Posee internet en el local?:" :options="['Si', 'No']" name="internet"
-            v-model="properties.internet" />
-          <InputCheckbox text="Métodos de pago a usar:"
-            :options="['Efectivo', 'Dolares', 'Tarjeta', 'Sinpe Movil', 'Credito', 'Pinpad']" name="metodopago"
-            v-model="properties.metodopago">
-          </InputCheckbox>
-          <InputCheckbox text="Para qué desea el sistema?:" :options="['Vender', 'Llevar inventarios']" name="sistema"
-            v-model="properties.sistema">
-          </InputCheckbox>
-          <InputCheckbox v-show="properties.sistema.includes('Llevar inventarios')"
-            text="Cómo desea llevar el inventario?:" :options="['Por unidades', 'Por gramaje(Recetas)']"
-            name="inventario" v-model="properties.inventario">
-          </InputCheckbox>
-          <InputCheckbox text="Cuáles de los siguientes aspectos son más importantes para usted?:" name="expectativas"
-            :options="['Mejorar el tiempo de cobro', 'Optimizar el control de estadísticas y ventas', 'Fortalecer el control de empleados', 'Mejorar la integración con la tecnología']"
-            v-model="properties.expectativas">
-          </InputCheckbox>
+          <InputText text="Persona de contacto:" placeholder="Nombre completo" name="contacto" v-model="properties.contacto" />
+          <InputText text="Nombre del comercio:" placeholder="Nombre de fantasía" name="fantasia" v-model="properties.fantasia" />
+          <InputText text="Razón social:" placeholder="Nombre físico o jurídico" name="razonsocial" v-model="properties.razonsocial" />
+          <InputNumber text="Número de cédula física o jurídica:" placeholder="101230321" name="cedula" v-model="properties.cedula" />
+          <RadioButton text="Régimen del negocio:" :options="['Simplificado', 'Tradicional']" name="regimen" v-model="properties.regimen" />
+          <InputDatalist text="Tipo de negocio" :options="['Soda', 'Restaurante', 'Cafeteria', 'Supermercado', 'Salon de belleza', 'Tienda de ropa']" name="tiponegocio" v-model="properties.tiponegocio"></InputDatalist>
+          <RadioButton text="Posee internet en el local?:" :options="['Si', 'No']" name="internet" v-model="properties.internet" />
+          <InputCheckbox text="Métodos de pago a usar:" :options="['Efectivo', 'Dolares', 'Tarjeta', 'Sinpe Movil', 'Credito', 'Pinpad']" name="metodopago" v-model="properties.metodopago"></InputCheckbox>
+          <InputCheckbox text="Para qué desea el sistema?:" :options="['Vender', 'Llevar inventarios']" name="sistema" v-model="properties.sistema"></InputCheckbox>
+          <InputCheckbox v-show="properties.sistema.includes('Llevar inventarios')" text="Cómo desea llevar el inventario?:" :options="['Por unidades', 'Por gramaje(Recetas)']" name="inventario" v-model="properties.inventario"></InputCheckbox>
+          <InputCheckbox text="Cuáles de los siguientes aspectos son más importantes para usted?:" name="expectativas" :options="['Mejorar el tiempo de cobro', 'Optimizar el control de estadísticas y ventas', 'Fortalecer el control de empleados', 'Mejorar la integración con la tecnología']" v-model="properties.expectativas"></InputCheckbox>
           <button type="submit" class="bg-green-400 p-2 m-2 w-28 rounded-md self-center">Descargar</button>
         </form>
       </div>
     </div>
   </div>
 </template>
+
 <script setup>
 import { ref, nextTick } from "vue";
 import InputText from "@/components/InputText.vue";
